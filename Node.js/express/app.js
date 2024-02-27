3792afd079e1d93f85ed2bc1874d6610d6b48f11
Patryk Sąsiadek <patrykskontakt@gmail.com>
2024-02-27 19:50:45 +0100

    express basics

--- Node.js/express/app.js
+++ Node.js/express/app.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+const rootDir = require('./utils/path');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(rootDir, 'public')));
 
 app.use(`/admin`, adminRoutes);
 app.use(shopRoutes);
+
 app.use((req, res, next) => {
-	res.status(404).send('<h1>Page not found!</h1>');
+	res.status(404).sendFile(path.join(__dirname, 'views', 'not-found.html'));
 });
 
 app.listen(3000);
