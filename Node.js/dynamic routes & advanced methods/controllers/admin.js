9378c9ccd56015da60ca871fe54b0bb9bf8702ad
Patryk Sąsiadek <patrykskontakt@gmail.com>
2024-03-13 13:43:00 +0100

    edit route

--- Node.js/dynamic routes & advanced methods/controllers/admin.js
+++ Node.js/dynamic routes & advanced methods/controllers/admin.js
@@ -1,31 +1,47 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
-  });
+	res.render('admin/edit-product', {
+		pageTitle: 'Add Product',
+		path: '/admin/add-product',
+		editing: false,
+	});
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const title = req.body.title;
-  const imageUrl = req.body.imageUrl;
-  const price = req.body.price;
-  const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+	const title = req.body.title;
+	const imageUrl = req.body.imageUrl;
+	const price = req.body.price;
+	const description = req.body.description;
+	const product = new Product(title, imageUrl, description, price);
+	product.save();
+	res.redirect('/');
+};
+
+exports.getEditProduct = (req, res, next) => {
+	const editMode = req.query.edit;
+	const productId = req.params.productId;
+
+	Product.findById(productId, product => {
+		if (!product) {
+			return res.redirect('/admin/products');
+		}
+
+		res.render('admin/edit-product', {
+			pageTitle: 'Edit Product',
+			path: '/admin/products',
+			editing: editMode,
+			product,
+		});
+	});
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
-    });
-  });
+	Product.fetchAll(products => {
+		res.render('admin/products', {
+			prods: products,
+			pageTitle: 'Admin Products',
+			path: '/admin/products',
+		});
+	});
 };
