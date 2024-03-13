4e3cb5f175bd3147f04ef46a3e845b1fbee76400
Patryk Sąsiadek <patrykskontakt@gmail.com>
2024-03-13 01:17:33 +0100

    add to cart brief

--- Node.js/dynamic routes & advanced methods/controllers/shop.js
+++ Node.js/dynamic routes & advanced methods/controllers/shop.js
@@ -14,10 +14,12 @@ exports.getProduct = (req, res, next) => {
 	const productId = req.params.productId;
 
 	Product.findById(productId, product => {
-		console.log(product);
+		res.render('shop/product-detail', {
+			pageTitle: product.title,
+			path: `/products`,
+			product,
+		});
 	});
-
-	res.redirect('/');
 };
 
 exports.getIndex = (req, res, next) => {
@@ -35,6 +37,12 @@ exports.getCart = (req, res, next) => {
 		path: '/cart',
 		pageTitle: 'Your Cart',
 	});
+};
+
+exports.postCart = (req, res, next) => {
+	const productId = req.body.productId;
+	console.log(productId);
+	res.redirect('/cart');
 };
 
 exports.getOrders = (req, res, next) => {
