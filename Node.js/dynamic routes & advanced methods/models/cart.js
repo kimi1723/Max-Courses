c4d79e2f1b673a4ec76286a25d73e8031c95ed83
Patryk Sąsiadek <patrykskontakt@gmail.com>
2024-03-13 18:39:48 +0100

    cart display

--- Node.js/dynamic routes & advanced methods/models/cart.js
+++ Node.js/dynamic routes & advanced methods/models/cart.js
@@ -59,4 +59,15 @@ module.exports = class Cart {
 			});
 		});
 	}
+
+	static getCart(cb) {
+		fs.readFile(path, (err, fileContent) => {
+			const cart = JSON.parse(fileContent);
+			if (err) {
+				cb(null);
+			} else {
+				cb(cart);
+			}
+		});
+	}
 };
