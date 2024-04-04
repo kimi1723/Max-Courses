bf4b2de60817df636775898390d9c8daf1a36f63
Patryk Sąsiadek <patrykskontakt@gmail.com>
2024-04-04 23:39:31 +0200

    connectMongo

--- Node.js/dynamic routes & advanced methods/app.js
+++ Node.js/dynamic routes & advanced methods/app.js
@@ -1,20 +1,15 @@
 require('dotenv').config();
 
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-
 const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const sequelize = require('./util/database');
+const connectMongo = require('./util/database');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
 
 const app = express();
 
@@ -25,64 +20,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(async (req, res, next) => {
-	try {
-		const user = await User.findByPk(6);
+	// try {
+	// 	const user = await User.findByPk(6);
+	// 	req.user = user;
+	// 	next();
+	// } catch (err) {
+	// 	console.log(err);
+	// }
 
-		req.user = user;
-
-		next();
-	} catch (err) {
-		console.log(err);
-	}
+	next();
 });
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+// app.use('/admin', adminRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-
-// sequelize
-// 	// .sync({ force: true })
-// 	.sync()
-// 	.then(res => User.findByPk(6))
-// 	.then(user => {
-// 		if (!user) {
-// 			User.create({ name: 'Max', email: 'test@test.com', password: 'test123' });
-// 		}
-// 		console.log(user);
-// 		return Promise.resolve(user);
-// 	})
-// 	.then(user => {
-// 		console.log(user);
-// 		app.listen(3000);
-// 	})
-// 	.catch(err => {
-// 		console.log(err);
-// 	});
-
-const syncSequelize = async () => {
-	try {
-		await sequelize.sync();
-
-		const user = await User.findByPk(6);
-
-		if (!user) {
-			User.create({ name: 'Max', email: 'test@test.com', password: 'test123' });
-		}
-
-		await user.createCart();
-
-		app.listen(3000);
-	} catch (err) {
-		console.log(err);
-	}
-};
-
-syncSequelize();
+connectMongo(client => {
+	app.listen(3000);
+});
--- Node.js/dynamic routes & advanced methods/util/database.js
+++ Node.js/dynamic routes & advanced methods/util/database.js
@@ -1,8 +1,30 @@
-const Sequelize = require('sequelize').Sequelize;
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete-course', 'root', process.env.DB_PASSWORD, {
-	dialect: 'mysql',
-	host: 'localhost',
-});
+let _db;
 
-module.exports = sequelize;
+const connectMongo = async callback => {
+	try {
+		const client = await MongoClient.connect(
+			`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@learning.p3olyab.mongodb.net/shop`,
+			// ?retryWrites=true
+		);
+
+		_db = client.db();
+
+		callback(client);
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+const getDb = () => {
+	if (_db) {
+		return _db;
+	}
+
+	throw 'No database found!';
+};
+
+exports.connectMongo = connectMongo;
+exports.getDb = getDb;
