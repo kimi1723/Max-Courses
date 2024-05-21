1084428d64c83cf7496805631222182c08cfe359
Patryk Sąsiadek <patrykskontakt@gmail.com>
2024-05-21 12:38:33 +0200

    enum

--- TypeScript/getting started/index.js
+++ TypeScript/getting started/index.js
@@ -1,6 +1,14 @@
 var submitBtn = document.getElementById('submit');
 var num1 = document.getElementById('num1');
 var num2 = document.getElementById('num2');
+var Direction;
+(function (Direction) {
+    Direction[Direction["Up"] = 1] = "Up";
+    Direction[Direction["Down"] = 2] = "Down";
+    Direction[Direction["Left"] = 3] = "Left";
+    Direction[Direction["Right"] = 4] = "Right";
+})(Direction || (Direction = {}));
+var dir = { up: 1, down: 2 };
 var add = function (num1, num2) {
     console.log(num1 + num2);
 };
--- TypeScript/getting started/index.ts
+++ TypeScript/getting started/index.ts
@@ -2,6 +2,15 @@ const submitBtn = document.getElementById('submit') as HTMLButtonElement;
 const num1 = document.getElementById('num1') as HTMLInputElement;
 const num2 = document.getElementById('num2') as HTMLInputElement;
 
+enum Direction {
+	Up = 1,
+	Down,
+	Left,
+	Right,
+}
+
+const dir = { up: 1, down: 2 } as const;
+
 const add = (num1: number, num2: number) => {
 	console.log(num1 + num2);
 };
